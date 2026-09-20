0b8aae2a93bac5bb5f5a54861c22356c2adf1d15
ricardo herbas <ricardosebaherbas@gmail.com>
2026-09-20 05:58:27 +0000

    arreglos

--- backend/routes/ia.route.js
+++ backend/routes/ia.route.js
@@ -4,6 +4,5 @@ const iaController = require('../controllers/ia.controller')
 const { consultaValidator } = require('../middlewares/ia-validator')
 const { verificarToken } = require('../middlewares/auth.middleware')
 
-router.post('/consultar', consultaValidator, iaController.consultarIA)
-//verificarToken,
+router.post('/consultar',verificarToken, consultaValidator, iaController.consultarIA)
 module.exports = router
